c76ea150bf0fd04bec9f24e79f84ef46961efe3a
Diep Huu Hoang <hoang@trobz.com>
2023-09-19 15:01:25 +0700

    [IMP] stock_release_channel_process_end_time: pre-commit issue

--- stock_release_channel_process_end_time/models/res_config_settings.py
+++ stock_release_channel_process_end_time/models/res_config_settings.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, SUPERUSER_ID, _
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     update_scheduled_date = fields.Boolean(
         help="Will update scheduled date of picking based on process end date(time).",
-        config_parameter='stock_release_channel_process_end_time.update_scheduled_date'
+        config_parameter="stock_release_channel_process_end_time.update_scheduled_date",
     )
